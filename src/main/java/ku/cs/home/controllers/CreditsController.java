485f7a19c4a117d5d103a4cd5337eec3f66b768a
Punnatud <punnatud.d@ku.th>
2022-08-10 22:26:00 +0700

    add nokkokPicture

--- src/main/java/ku/cs/home/controllers/CreditsController.java
+++ src/main/java/ku/cs/home/controllers/CreditsController.java
@@ -2,6 +2,8 @@ package ku.cs.home.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
@@ -14,5 +16,11 @@ public class CreditsController {
             System.err.println("ไปที7หน้า home ไม่ได้");
             System.err.println("ให้ตรวจสอบการกําหนด route");
         }
+    }
+
+    @FXML private ImageView nokkokImage;
+    public void initialize(){
+        String url = getClass().getResource("/ku/cs/images/nokkok.jpg").toExternalForm();
+        nokkokImage.setImage(new Image(url));
     }
 }
