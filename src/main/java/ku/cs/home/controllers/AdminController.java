ac72a2d0d40a76691b8b923fdbdecdd902e6dec3
lemonstonex <lemonstonex@gmail.com>
2022-09-04 21:27:07 +0700

    change home.fxml color code

--- src/main/java/ku/cs/home/controllers/AdminController.java
+++ src/main/java/ku/cs/home/controllers/AdminController.java
@@ -15,4 +15,14 @@ public class AdminController {
             System.err.println("ให้ตรวจสอบการกําหนด route");
         }
     }
+
+    @FXML
+    public void handleLogoutButton(ActionEvent actionEvent) {
+        try {
+            com.github.saacsos.FXRouter.goTo("home");
+        } catch (IOException e) {
+            System.err.println("ไปที่หน้า home ไม่ได้");
+            System.err.println("ให้ตรวจสอบการกําหนด route");
+        }
+    }
 }
