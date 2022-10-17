0795725aa27b659da86e8584fb23f15fd2a121a1
Punnatud <punnatud.d@ku.th>
2022-10-17 17:23:52 +0700

    create filterer and filterBy method in ComplaintList

--- src/main/java/ku/cs/home/controllers/ReportInDetailController.java
+++ src/main/java/ku/cs/home/controllers/ReportInDetailController.java
@@ -31,7 +31,6 @@ public class ReportInDetailController {
     @FXML private ComboBox reportTypeBox;
     @FXML private ComboBox subjectBox;
     @FXML private TextArea detailTextArea;
-    @FXML private Button submitButton;
 
 
     private String type;
